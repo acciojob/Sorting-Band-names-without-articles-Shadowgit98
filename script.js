1d1bd58f7facc42ab4ebab5c2454be8465502d07
Prathmesh Gandhale <125843181+Shadowgit98@users.noreply.github.com>
2023-06-15 09:49:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 //your code here
+function sortBandNames(bandNames) {
+  // Define the list of articles to be excluded
+  const articles = ['a', 'an', 'the'];
+
+  // Remove articles from band names and store in a new array
+  const sanitizedBandNames = bandNames.map((name) => {
+    // Split the name into individual words
+    const words = name.split(' ');
+
+    // Filter out articles from the words array
+    const filteredWords = words.filter((word) => !articles.includes(word.toLowerCase()));
+
+    // Join the filtered words to form the sanitized name
+    const sanitizedName = filteredWords.join(' ');
+
+    return sanitizedName;
+  });
+
+  // Sort the sanitized band names in lexicographic order
+  const sortedBandNames = sanitizedBandNames.sort((a, b) => a.localeCompare(b));
+
+  // Generate the HTML for displaying the sorted band names
+  const html = sortedBandNames.map((name) => `<li>${name}</li>`).join('');
+
+  // Return the HTML string
+  return html;
+}
+
+// Example usage
+const bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+const sortedBandsHTML = sortBandNames(bandNames);
+const ulElement = `<ul id="band">${sortedBandsHTML}</ul>`;
+console.log(ulElement);
 
